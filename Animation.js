86e4f953c85a022f05faf9f91c9a6a48faaffa83
Abedelrahman Sholi <s.holi_1989@live.com>
2023-07-24 12:25:39 +0300

    Organize the files and add onclick stop animation

--- Animation.js
+++ Animation.js
@@ -1,3 +1,15 @@
+let animationTimeout; // Variable to store the animation timeout
+
+const greetingElement = document.getElementById('greeting');
+const summeryElement = document.getElementById('summery');
+const detailsElement = document.getElementById('details');
+
+const greetingMSG = "Hey, I'm Abedelrahman Sholi";
+const summeryMSG = "Professional Quality Assurenace Engineer.";
+const detailsMSG =
+  "Results-driven engineer, leading cross-functional teams for successful product rollouts, achieving high-quality software releases, and optimizing efficiency.";
+
+
 // Define the animate function with a Promise-based approach
 function animate(str, startIndex = 0, elementInHTML) {
   return new Promise((resolve) => {
@@ -9,7 +21,7 @@ function animate(str, startIndex = 0, elementInHTML) {
       elementInHTML.innerHTML = text;
 
       if (text === str) {
-        resolve();
+        resolve(); // Resolve the Promise when the animation completes
       } else {
         const char = text.slice(-1);
         if (char === '<') isTag = true;
@@ -20,7 +32,7 @@ function animate(str, startIndex = 0, elementInHTML) {
           animateText();
         } else {
           startIndex++;
-          setTimeout(animateText, 80);
+          animationTimeout = setTimeout(animateText, 80); // Set the animationTimeout variable
         }
       }
     }
@@ -30,23 +42,30 @@ function animate(str, startIndex = 0, elementInHTML) {
 }
 
 async function animateOnVisible() {
-  const greetingElement = document.getElementById('greeting');
-  const summeryElement = document.getElementById('summery');
-  const detailsElement = document.getElementById('details');
-
-  var greetingMSG = "Hey, I'm Abedelrahman Sholi";
-  var summeryMSG = "Professional Quality Assurenace Engineer.";
-  var detailsMSG =
-    "Results-driven engineer, leading cross-functional teams for successful product rollouts, achieving high-quality software releases, and optimizing efficiency.";
-
   await animate(greetingMSG, 0, greetingElement);
   await animate(summeryMSG, 0, summeryElement);
   await animate(detailsMSG, 0, detailsElement);
+  await clearHint();
+}
+
+function clearHint(){
+  const hintElement = document.getElementById('hint');
+  hintElement.innerHTML = "";
 }
 
 // will be executed once the file loaded
 animateOnVisible();
 
+// Clear the animation timeout to stop the animation
+// This will stop the animation and make the text appear
+document.addEventListener('click', function () {
+  clearTimeout(animationTimeout);
+
+  greetingElement.innerHTML = greetingMSG;
+  summeryElement.innerHTML = summeryMSG;
+  detailsElement.innerHTML = detailsMSG;
+  clearHint();
+});
 
 //Code used for navigation bar elements
 // Setting up the Variables
@@ -59,8 +78,8 @@ function barClicked() {
   bars.classList.toggle('active');
   nav.classList.toggle('visible');
 }
-
-function closeMenu(){
+// Remove effect
+function closeMenu() {
   bars.classList.remove('active');
   nav.classList.remove('visible');
 }
